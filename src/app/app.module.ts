920a9a5d0f5d0da5742680de26f4014357243f7b
davidaf22 <davidaf22@hotmail.es>
2020-02-13 13:23:35 +0100

    Segundo Commit, creación del componente listo-pructos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductoComponent } from './producto/producto.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductoComponent
+    ProductoComponent,
+    ListaProductosComponent
   ],
   imports: [
     BrowserModule
