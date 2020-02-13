92ebdc7785a27c4c662fc0ffc983cc37f01f252e
davidaf22 <davidaf22@hotmail.es>
2020-02-13 22:43:41 +0100

    Introducimos un sistema de selección con imputs tipo "radio" para cambiar la variable "comprado" asi como recorrer nuevamente el array de productos para mostrar los comprados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductoComponent } from './producto/producto.component';
 import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductoComponent,
-    ListaProductosComponent
+    ListaProductosComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/lista-productos/lista-productos.component.spec.ts
+++ src/app/lista-productos/lista-productos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ListaProductosComponent } from './lista-productos.component';
 
